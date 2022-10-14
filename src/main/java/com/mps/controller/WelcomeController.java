72c9691cf0df8c48cdc2e8f01ac2f215307d3938
mohitprakash <mohitprakash@localhost>
2022-10-15 01:23:24 +0530

    STS-commit getMsg url-pattern

---
+++ src/main/java/com/mps/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.mps.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/wel")
+	public ResponseEntity<String> getMsg()
+	{
+		String msg="Hello World";
+		return new ResponseEntity<>(msg,HttpStatus.OK);
+	}
+	
+}
